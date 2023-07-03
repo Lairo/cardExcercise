bcc414d25684a294629ae239ef5f65396b48632a
chajus1 <chajus1@googlemail.com>
2023-07-03 19:50:58 +0100

    testing lists

--- cardExcercise/Program.cs
+++ cardExcercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cardExcercise
 {
@@ -9,21 +10,25 @@ namespace cardExcercise
         static void Main()
         {
             Card myCard = new Card(Suit.Spades, Value.Ace);
-            Console.WriteLine(myCard.Name);
+            //Console.WriteLine(myCard.Name);
 
-            
-            
-           
+            //int count = 0;
+            //while (count != 20)
+            //{
+            //    int numberBetween0and3 = random.Next(4);
+            //    int numberBetween1and13 = random.Next(1, 14);
+            //    Card randomCard = new Card((Suit)numberBetween0and3, (Value)numberBetween1and13);
+            //    Console.WriteLine($"{count+1} {randomCard.Name}");
+            //    count++;
+            //}
+            List<Card> cards = new List<Card>();
 
-            int count = 0;
-            while (count != 20)
-            {
-                int numberBetween0and3 = random.Next(4);
-                int numberBetween1and13 = random.Next(1, 14);
-                Card randomCard = new Card((Suit)numberBetween0and3, (Value)numberBetween1and13);
-                Console.WriteLine($"{count+1} {randomCard.Name}");
-                count++;
-            }
+            cards.Add(new Card(Suit.Diamonds, Value.King));
+            cards.Add(new Card((Suit)2, (Value)3));
+            cards.Add(new Card(Suit.Hearts, Value.Ace));
+            Console.WriteLine(cards[0].Name);
+            Console.WriteLine(cards[1].Name);
+            Console.WriteLine(cards[2].Name);
 
         }
     }
